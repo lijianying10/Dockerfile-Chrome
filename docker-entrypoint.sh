f8c52fac1b11e52801946f1c77988f2e56170d48
Li jianying <lijianying12@gmail.com>
2017-12-23 15:44:11 +0800

    升级启动脚本，修改位置了。

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,8 @@ socat tcp-listen:80,fork tcp:localhost:9222 &
 echo 'start chrome'
 if [[ -z "${PROXY}" ]]; then
   echo 'chrome with out proxy'
-  /chrome-linux/chrome --remote-debugging-port=9222  --headless  --no-sandbox
+  /usr/bin/chromium --remote-debugging-port=9222 --headless --no-sandbox --disable-gpu
 else
   echo 'chrome with proxy' $PROXY
-  /chrome-linux/chrome --remote-debugging-port=9222  --headless  --no-sandbox --proxy-server=$PROXY
+  /usr/bin/chromium --remote-debugging-port=9222 --headless --no-sandbox --disable-gpu --proxy-server=$PROXY
 fi
